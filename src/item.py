2afccaa7497f50cdd82e002857071e376ac6177a
daniilmurzin333 <daniilmurzin333@gmail.com>
2023-06-07 23:23:40 +0300

    Homework.1 переписаны тесты и исправлена ошибка item.all

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import os
+from csv import DictReader
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,12 +17,10 @@ class Item:
                 :param price: Цена за единицу товара.
                 :param quantity: Количество товара в магазине.
                 """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
-
-
 
     def calculate_total_price(self):
         """
@@ -28,11 +30,48 @@ class Item:
         """
         return self.price * self.quantity
 
-
     def apply_discount(self):
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            raise Exception('Длина товара превышает 10 символов')
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """
+        Создает экземпляры класса на основе таблицы хранящейся в items.csv
+        """
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(dir_path + '/items.csv', encoding='utf-8') as csv_file:
+            csv_reader = DictReader(csv_file)
+            cls.all = []
+            for (row) in csv_reader:
+                name = row['name']
+                price = row['price']
+                quantity = row['quantity']
+                cls(name, float(price), int(quantity))
+
+    @staticmethod
+    def string_to_number(number: str) -> int:
+        """
+        Преобразовывает строки-числа в числа
+        """
+        if "." in number:
+            return int(float(number))
+        else:
+            return int(number)
+
+
 
 
--- tests/test_item.py
+++ tests/test_item.py
@@ -3,7 +3,9 @@ from src.item import Item
 
 item1 = Item("Смартфон", 1000, 3)
 item2 = Item("Ноутбук", 2500, 2)
+item3 = Item("Микроволновка", 8000, 6)
 Item.pay_rate = 0.8
+
 
 
 def test_item_init():
@@ -23,4 +25,20 @@ def test_calculate_total_price():
 def test_apply_discount():
     assert item1.apply_discount() is None
     assert item2.apply_discount() is None
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_name():
+    assert len(item2.name) <= 10
+    assert len(item3.name) >= 10
 
