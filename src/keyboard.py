7ca73a1959908c0dea7ac24f2ffcbf83801ca940
daniilmurzin333 <daniilmurzin333@gmail.com>
2023-06-26 15:56:35 +0300

    Homework-4 - создан новый класс и написаны тесты к нему

---
+++ src/keyboard.py
@@ -0,0 +1,26 @@
+from src.item import Item
+
+
+class MixinLanguage:
+    __slots__ = ['__language']
+
+    def __init__(self) -> None:
+        self.__language: str = "EN"
+
+    @property
+    def language(self) -> str:
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == 'EN':
+            self.__language = 'RU'
+        else:
+            self.__language = 'EN'
+        return self
+
+
+class Keyboard(Item, MixinLanguage):
+    __slots__ = ['name', 'price', 'quantity']
+
+    def __init__(self, name: str, price: float, quantity: int) -> None:
+        super().__init__(name, price, quantity)
